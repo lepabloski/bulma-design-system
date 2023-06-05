5ca5f84153551e5261d12ee369239363d5f5e0c4
Pablo Cabrol <pablo.cabrol@softvision.com>
2023-06-05 12:04:31 -0300

    Install Commit

---
+++ projects/design-system/src/lib/design-system.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-designSystem',
+  template: `
+    <p>
+      design-system works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DesignSystemComponent {
+
+}
